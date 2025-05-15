0ecadff55e64be6544d16bcd08e13afcf2355082
developer1762 <develop1762@gmail.com>
2025-05-15 12:18:04 +0530

    Build files uploaded

---
+++ precache-manifest.4fbe366bbd6f40f6fe2faef49c25d019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3761d6c3d8d135d7832e",
+    "url": "/json_pdf_tool/static/css/main.4fc90965.chunk.css"
+  },
+  {
+    "revision": "3761d6c3d8d135d7832e",
+    "url": "/json_pdf_tool/static/js/main.0b45f4b1.chunk.js"
+  },
+  {
+    "revision": "f7da3b9997488e83d7db",
+    "url": "/json_pdf_tool/static/js/runtime~main.da29bb5e.js"
+  },
+  {
+    "revision": "b7e37b3b4baa76d0017f",
+    "url": "/json_pdf_tool/static/js/2.9d0e09ba.chunk.js"
+  },
+  {
+    "revision": "5bbbafe601a1fa672436",
+    "url": "/json_pdf_tool/static/js/3.a40f4158.chunk.js"
+  },
+  {
+    "revision": "3fa0c45e58c9ed14b85692406625bbad",
+    "url": "/json_pdf_tool/index.html"
+  }
+];
