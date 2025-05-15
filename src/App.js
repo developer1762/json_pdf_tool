3ab3b5c5d8626fc7276bd39978ba5dbdb72de2a6
atulsharma762 <atul762@gmail.com>
2025-05-15 14:02:18 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import './App.css';
 
-import Header from './Header';
-import Footer from './Footer';
-import Body from './Body';
+import Header from './components/Header';
+
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Convert from './pages/Convert';
+import View from './pages/View';
 
 export default function App() {
   return (
-    <Box sx={{ backgroundColor: '#f4f4f4', overflowY:'hidden' }}>
-      <Header />
+    <Router>
+      <Box sx={{ backgroundColor: '#f4f4f4', display: 'flex', flexDirection: 'column', maxWidth: '100vw' , overflowX: 'hidden'}}>
+        <Header />
 
-      <Body/>
+        <Container sx={{ flex: 1, py: 1}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/convert" element={<Convert />} />
+            <Route path="/view" element={<View />} />
+          </Routes>
+        </Container>
 
-      <Footer />
-    </Box>
+        <Footer />
+      </Box>
+    </Router>
   );
 }
-
